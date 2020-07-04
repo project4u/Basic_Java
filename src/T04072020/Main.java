94956de26596ca9ae886747a70e99fd6904f4d84
unknown <ds49574.ds@gmail.com>
2020-07-04 16:00:24 +0530

    T04072020 Comparing two objects

--- src/T04072020/Main.java
+++ src/T04072020/Main.java
@@ -1,4 +1,25 @@
 package T04072020;
 
 public class Main {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.setAge(10);
+        s1.setFirstname("F1");
+        s1.setLastname("L1");
+        s1.setUserId("12345");
+        Student s2=new Student();
+        s2.setUserId("12345");
+        s2.setFirstname("F2");
+        s2.setLastname("L2");
+        s2.setAge(12);
+        boolean state=s1.equals(s2);
+        // here equality check on Student, so equals will check the equal method in Student class
+        // to check on what basis two Object are equal. in this project we are considering two objects equal if their
+        // userId is same. you can check equality of two object based on userid along with student firstname and lastname.
+        System.out.println("two Objects are :" +state);
+        System.out.println(s1);
+        System.out.println(s2.toString());
+        // This code is for understanding of equals and toString method overidden in Student class.
+    }
+
 }
--- src/T04072020/Student.java
+++ src/T04072020/Student.java
@@ -1,4 +1,54 @@
 package T04072020;
 
 public class Student {
+    String firstname;
+    String lastname;
+    String userId;
+    int age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Student s=(Student)obj;
+        if(this.userId.equals(s.userId))
+            return true;
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return getUserId()+ " "+ getFirstname()+" "+getLastname()+" "+getAge();
+        //return super.toString();
+    }
 }
